9f35104620765cd3f12b2273f31cd6063e3496f9
DARKINGER <alexanfmdm@gmail.com>
2023-08-30 18:10:59 -0500

    parcial2

---
+++ Parcial 2/testoAle/index.js
@@ -0,0 +1,15 @@
+let frases = [
+    "Interesante Uno",
+    "Interesante dos",
+    "Interesante tres"
+]
+
+
+export function obtienefrase(indice){
+    return frases[indice]; 
+}
+
+//console.log(module);
+
+// module.exports.obtienefrase=obtienefrase;
+
