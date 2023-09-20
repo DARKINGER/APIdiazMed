3be7badf464137f97ece31a0332f304ad012bfb6
DARKINGER <alexanfmdm@gmail.com>
2023-09-20 07:25:00 -0500

    eventos

---
+++ unidad 1/Eventos/eventos.js
@@ -0,0 +1,24 @@
+
+events = require('events');
+emisor = new events.EventEmitter();
+emisor.on('Saludar', (nombre)=>{console.log('hola '+nombre)});
+
+emisor.emit('Saludar','jose');
+
+
+function saludar(){
+    const emisor = new events.EventEmitter();
+    setTimeout(()=>emisor.emit('Saludar', 'nono'),5000);
+    setTimeout(()=>emisor.emit('Saludar', 'nana'),500);
+    setTimeout(()=>emisor.emit('Saludar', 'nora'),2000);
+    return emisor
+}
+
+let sal = saludar();
+
+sal.on('Saludar',(nombre)=>{
+    console.log('hola '+nombre)
+})
+
+
+
